c300a6ade923e0b9397eba7d78a895f28cef9469
Omcevoy <omcevoy@uoregon.edu>
2019-07-10 15:47:59 -0700

    updated site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,10 @@
 node ip-10-0-4-206 {
+#update /etc/puppet and run "puppet apply":
+	cron { "puppet apply":
+		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
+		user => "root",
+		minute => "*/5",		
+	}
 }
-
 node ip-10-0-4-128 {
 }
