a85fbb1cf77d04165f1be5b9703b2a15fdabd39a
Omcevoy <omcevoy@uoregon.edu>
2019-07-11 15:37:10 -0700

    adding code directory

---
+++ code/modules/sshd/manifests/site.pp
@@ -0,0 +1,18 @@
+class sshd {
+	package { "openssh-server":
+		ensure => installed;	
+	}
+	file { "/etc/ssh/sshd_config":
+		ensure  => present,
+		mode    => '444',
+		owner   => 'root',
+		group   => 'root',
+		source  => "puppet:///modules/sshd/sshd_config",	
+		require => Package["openssh-server"], 
+	}	
+
+	service { "ssh":
+		enable    => true,
+		ensure    => running, 
+		subscribe => File["/etc/ssh/sshd_config"],
+	}
