2bfb150538336fd16cbef08db9256066b4fb42a7
Omcevoy <omcevoy@uoregon.edu>
2019-07-13 12:16:46 -0700

    now instances will include puppet

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,6 @@
 node ip-10-0-4-206 {
-#update /etc/puppet and run "puppet apply":
-	cron { "puppet apply":
-		command => "cd /etc/puppet && git pull -q origin master && puppet apply manifests/site.pp",
-		user => root,
-		minute => "*/5",		
-	}
+	include puppet
 }
 node ip-10-0-4-128 {
+	include puppet
 }
