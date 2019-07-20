b3c94730274322dc000bd245c4e7ba8f87e37145
Omcevoy <omcevoy@uoregon.edu>
2019-07-20 12:56:28 -0700

    added apache directory and initial puppet code. To be tested ...

---
+++ code/modules/apache/manifests/init.pp
@@ -0,0 +1,21 @@
+class apache {
+	package { "apache2":
+		ensure   => installed,
+		provider => apt,
+	}
+	file {"/etc/apache2/apache2.conf":
+		ensure  => present,
+		mode    => '444',
+		owner   => 'root', 
+		group   => 'root', 
+		source  => "puppet:///modules/apache/apache2.conf",
+		require => Package["apache2"],
+	}
+	service { "apache":
+		enable => true,
+		ensure => running, 
+		subscribe => File["/etc/apache2/apache2.conf"],
+		require   => [ Package ['apache2'], ],
+	}
+
+}
