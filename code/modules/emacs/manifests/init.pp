186f1e8c8e9fcece84b6e04a8a1a520a9c8596b9
Omcevoy <omcevoy@uoregon.edu>
2019-08-11 12:23:39 -0700

    adding emacs

---
+++ code/modules/emacs/manifests/init.pp
@@ -0,0 +1,19 @@
+class emacs {
+	package { "emacs":
+		ensure => installed,
+	}
+	
+	file {"/etc/emacs/site-start.d/00debian-vars.el":
+		ensure  => present,
+		mode    => '444',
+		owner   => 'root',
+		group   => 'root', 
+		source  => "puppet:///modules/emacs/00debian-vars.el",
+		require => Package["emacs"],
+	}
+	service { "emacs": 
+		enable    => true,
+		ensure    => running,
+		subscribe => File["/etc/emacs/site-start.d/00debian-vars.el"],
+	}
+}
