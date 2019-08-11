9dd21b9443087df6269963e88d99b69f5c8f576d
Omcevoy <omcevoy@uoregon.edu>
2019-08-11 12:25:24 -0700

    included emacs

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,12 @@ node ip-10-0-4-206 {
 	include sshd
 	include apache2
 	include user
+	include emacs
 }
 node ip-10-0-4-128 {
 	include puppet
 	include sshd
 	include apache2
 	include user
+	include emacs
 }
