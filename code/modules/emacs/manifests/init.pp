3e1005a4c997d1090b3d65afde7cfe7f93ab2ad3
Omcevoy <omcevoy@uoregon.edu>
2019-08-11 13:11:11 -0700

    more changes

--- code/modules/emacs/manifests/init.pp
+++ code/modules/emacs/manifests/init.pp
@@ -12,9 +12,4 @@ class emacs {
 		source  => "puppet:///modules/emacs/00debian-vars.el",
 		require => Package["emacs"],
 	}
-	service { "emacs": 
-		subscribe => File["/etc/emacs/site-start.d/00debian-vars.el"],
-		ensure    => "running",
-		enable    => "true",
-	}
 }
