1e3df6669c1d16b753cedee78796eeb8807b525e
Omcevoy <omcevoy@uoregon.edu>
2019-08-12 13:23:07 -0700

    removed stuff that didn't do anything

--- code/modules/emacs/manifests/init.pp
+++ code/modules/emacs/manifests/init.pp
@@ -3,13 +3,4 @@ class emacs {
 		ensure   => installed,
 		provider => apt, 
 	}
-	
-	file {"/etc/emacs/site-start.d/00debian-vars.el":
-		ensure  => present,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root', 
-		source  => "puppet:///modules/emacs/00debian-vars.el",
-		require => Package["emacs"],
-	}
 }
